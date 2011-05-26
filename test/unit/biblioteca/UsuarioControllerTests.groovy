287cd286978067eb78403de5a8d3ebffc2839df7
Canx <canchete@gmail.com>
2011-05-26 10:32:23 +0200

    Test handleLogin

--- test/unit/biblioteca/UsuarioControllerTests.groovy
+++ test/unit/biblioteca/UsuarioControllerTests.groovy
@@ -3,15 +3,22 @@ package biblioteca
 import grails.test.*
 
 class UsuarioControllerTests extends ControllerUnitTestCase {
+    def usuario1
+
     protected void setUp() {
         super.setUp()
+        usuario1 = new Usuario(login:"canchete",password:"holahola",nombre:"Ruben",apellidos:"Cancho Gasulla",tipo:"administrador")
+        mockDomain(Usuario, [usuario1])
     }
 
     protected void tearDown() {
         super.tearDown()
     }
 
-    void testSomething() {
-
+    void testHandleLogin() {
+        mockParams.login = usuario1.login
+        mockParams.password = usuario1.password
+        controller.handleLogin()
+        assertEquals controller.session.usuario, usuario1
     }
 }
